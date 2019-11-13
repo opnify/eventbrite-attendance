15b736827ddb85b17d4ca322d85cb2361afd1a98
Yudi <abdul.wahyudi@tokopedia.com>
2019-11-13 14:43:06 +0700

    Refactor: index.js "change to camelCase, new line, remove useless IIFE

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,72 +7,67 @@ import SEO from "../components/seo"
 
 QrScanner.WORKER_PATH = QrScannerWorkerPath;
 
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyilg571Q6cqJbmT1URr9d2S4FjCFM51_xY4EWR4URQE0p9JWw/exec";
+
 const debounce = (func, wait, immediate) => {
-  var timeout;
+  let timeout;
   return function() {
-    var context = this, args = arguments;
-    var later = function() {
+    let context = this,
+        args = arguments;
+    let later = function() {
       timeout = null;
       if (!immediate) func.apply(context, args);
     };
-    var callNow = immediate && !timeout;
+    let callNow = immediate && !timeout;
     clearTimeout(timeout);
     timeout = setTimeout(later, wait);
     if (callNow) func.apply(context, args);
   };
 };
 
-let timeStamp = (dateTime) => {
+const timeStamp = dateTime => {
   const padLeft = base => ('0' + base.toString()).slice(-2);
-
-  let dFormat = [ padLeft(dateTime.getMonth()+1),
-              padLeft(dateTime.getDate()),
-              dateTime.getFullYear()].join('/')+
-              ' at ' +
-              [ padLeft(dateTime.getHours()),
-                padLeft(dateTime.getMinutes())].join(':');
+  const dateFormat = [ padLeft(dateTime.getMonth()+1), padLeft(dateTime.getDate()), dateTime.getFullYear() ].join('/');
+  const timeFormat = [ padLeft(dateTime.getHours()), padLeft(dateTime.getMinutes()) ].join(':');
   
-  return dFormat;
+  return `${dateFormat} at ${timeFormat}`;
 };
 
 const IndexPage = () => {
-  const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyilg571Q6cqJbmT1URr9d2S4FjCFM51_xY4EWR4URQE0p9JWw/exec";
   const videoElem = useRef(null);
-  const QrResult = useRef(null);
+  const qrResultElem = useRef(null);
   const warning = useRef(null);
 
-  const [nameState, setNameState] = useState("");
+  const [name, setName] = useState("");
   const [showWarning, setShowWarning] = useState({"text": "User already registered", "show": false});
-
 
   useEffect(() => {
 
     // Timeout to default data
     const resetText = () => setTimeout(() => {
       setShowWarning({...showWarning, "show": false});
-      setNameState("");
+      setName("");
     }, 4000);
 
     // Add to local storage when dom loaded
-    (() => {
-      localStorage.clear();
-      if (!localStorage.getItem('users')) {
-        fetch(`${SCRIPT_URL}?action=read`)
-        .then(resp => resp.json())
-        .then(json => {
-          localStorage.setItem('users', JSON.stringify(json.records));
-        })
-        .catch(error => {
-          localStorage.setItem('users', '[]');
-        });
-      }
-    })();
+    localStorage.clear();
+    if (!localStorage.getItem('users')) {
+      fetch(`${SCRIPT_URL}?action=read`)
+      .then(resp => resp.json())
+      .then(json => {
+        localStorage.setItem('users', JSON.stringify(json.records));
+      })
+      .catch(error => {
+        localStorage.setItem('users', '[]');
+      });
+    }
 
     // Update attendance time in local storage
-    const updateStorage = (data) => {
-      let dataLocalStorage = JSON.parse(localStorage.getItem('users'));
-      let qrData = dataLocalStorage.map(record => record['Order #'].toString().includes(data)
-        ? { ...record, ['Attendance Time']: timeStamp(new Date()) } : record);
+    const updateStorage = data => {
+      const dataLocalStorage = JSON.parse(localStorage.getItem('users'));
+      const qrData = dataLocalStorage.map(record => record['Order #'].toString().includes(data)
+        ? { ...record, ['Attendance Time']: timeStamp(new Date()) }
+        : record);
       localStorage.setItem('users', JSON.stringify(qrData));
     }
 
@@ -81,15 +76,15 @@ const IndexPage = () => {
     let qrData = records.find(record => record['Order #'].toString().includes(data));
 
       if (qrData !== undefined) {
-        let qrCode = qrData['Order #'].toString();
-        let firstName = qrData['First Name'];
-        let lastName = qrData['Last Name'];
-        let AttendanceTime = qrData['Attendance Time'];
+        const qrCode = qrData['Order #'].toString();
+        const firstName = qrData['First Name'];
+        const lastName = qrData['Last Name'];
+        const attendanceTime = qrData['Attendance Time'];
 
-        if (records.length > 0 && data === qrCode && AttendanceTime === "") {
+        if (records.length > 0 && data === qrCode && attendanceTime === "") {
           updateData(data, records);
-          setNameState(`Welcome, ${firstName} ${lastName}`);
-        } else if (AttendanceTime !== "") {
+          setName(`Welcome, ${firstName} ${lastName}`);
+        } else if (attendanceTime !== "") {
           setShowWarning({...showWarning, "show": true});
         }
       } else {
@@ -101,8 +96,8 @@ const IndexPage = () => {
     };
 
     // Update data
-    const updateData = (data) => {
-      let formData = new FormData();
+    const updateData = data => {
+      const formData = new FormData();
       formData.append('Order #', data);
 
       fetch(`${SCRIPT_URL}?action=update`, {
@@ -123,7 +118,7 @@ const IndexPage = () => {
         data = data.toString().substring(0, 10);
         let dataLocalStorage = JSON.parse(localStorage.getItem('users'));
         checkCode(data, dataLocalStorage);
-      }else{
+      } else {
         clearTimeout(resetText());
         resetText();
       }
@@ -140,7 +135,7 @@ const IndexPage = () => {
       <SEO title="Home" />
       <h4 className="header-hint">Scan the QR code that has sent to your email</h4>
       <div style={{opacity: showWarning.show ? "1" : "0"}} ref={warning}>{showWarning.text}</div>
-      <div ref={QrResult}>{nameState != "" ? nameState : '\u00A0' }</div>
+      <div ref={qrResultElem}>{name != "" ? name : '\u00A0' }</div>
       <video id="qr-video" muted ref={videoElem}></video>
     </Layout>
   )
