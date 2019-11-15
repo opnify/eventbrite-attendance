c5c84c80904874240bd4337303307fdfdaa9b26e
Yudi91 <47311693+Yudi91@users.noreply.github.com>
2019-11-15 14:32:59 +0700

    Yudi/f eventbrite qrscanner (#2)
    * upload project
    
    * Refactor: index.js "change to camelCase, new line, remove useless IIFE
    
    * Feat: add toaster component, revamp design landing page, update text
    
    * deleted unused file scss

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.scss"
+import "./styles/layout.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -26,17 +26,8 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer></footer>
-      </div>
+      <main>{children}</main>
+      <footer></footer>
     </>
   )
 }
